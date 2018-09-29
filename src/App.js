0626f2363d926312b2633d32398c61f11e6f372f
Jimit Upadhyay <jimitupadhyay@gmail.com>
2018-09-29 01:29:00 -0400

    Added Layout Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
       <div >
-        
+        <Layout>
+            This is Main Layout...
+        </Layout>
       </div>
     );
   }

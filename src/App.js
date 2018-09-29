6956f7d12d8774a8836fc5066aac8a2e180c9c87
Jimit Upadhyay <jimitupadhyay@gmail.com>
2018-09-29 19:04:27 -0400

    Added Burger and BurgerIngredient - Dymanic Ingredients

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
     return (
       <div >
         <Layout>
-            This is Main Layout...
+            <BurgerBuilder />
         </Layout>
       </div>
     );
